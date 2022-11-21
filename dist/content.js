a94c30e6254643eefa36e05bfa25fc9a07c3fb3a
Manny Silva <hawkeyexl@gmail.com>
2022-11-20 20:50:41 -0800

    Dont update selector for dialog clicks

--- dist/content.js
+++ dist/content.js
@@ -434,10 +434,32 @@ __webpack_require__.r(__webpack_exports__);
 document.addEventListener("click", (event) => {
   let dialog = document.getElementById("doc-detective");
   if (dialog) {
+    const selector = (0,_medv_finder__WEBPACK_IMPORTED_MODULE_0__.finder)(event.target);
+    // Loop to identify if selector is part of main page
+    let inDialog = false;
+    let elements = [];
+    elements[0] = document.querySelector(selector);
+    for (let i = 0; i < elements.length; i++) {
+      let element = elements[i];
+      let parent = element.parentElement;
+      let parentName = parent.nodeName.toLowerCase();
+      if (parentName !== "body") {
+        elements.push(parent);
+        continue;
+      }
+      if (
+        parentName === "body" &&
+        element.nodeName.toLowerCase() === "div" &&
+        element.id === "doc-detective"
+      ) {
+        inDialog = true;
+      }
+    }
+    // Exit early if click is in the dialog
+    if (inDialog) return;
     let selectorDisplay = document.getElementById("selectorDisplay");
     event.stopPropagation();
     event.preventDefault();
-    const selector = (0,_medv_finder__WEBPACK_IMPORTED_MODULE_0__.finder)(event.target);
     selectorDisplay.innerHTML = selector;
   }
 });
--- src/content.js
+++ src/content.js
@@ -3,10 +3,32 @@ import { finder } from "@medv/finder";
 document.addEventListener("click", (event) => {
   let dialog = document.getElementById("doc-detective");
   if (dialog) {
+    const selector = finder(event.target);
+    // Loop to identify if selector is part of main page
+    let inDialog = false;
+    let elements = [];
+    elements[0] = document.querySelector(selector);
+    for (let i = 0; i < elements.length; i++) {
+      let element = elements[i];
+      let parent = element.parentElement;
+      let parentName = parent.nodeName.toLowerCase();
+      if (parentName !== "body") {
+        elements.push(parent);
+        continue;
+      }
+      if (
+        parentName === "body" &&
+        element.nodeName.toLowerCase() === "div" &&
+        element.id === "doc-detective"
+      ) {
+        inDialog = true;
+      }
+    }
+    // Exit early if click is in the dialog
+    if (inDialog) return;
     let selectorDisplay = document.getElementById("selectorDisplay");
     event.stopPropagation();
     event.preventDefault();
-    const selector = finder(event.target);
     selectorDisplay.innerHTML = selector;
   }
 });
