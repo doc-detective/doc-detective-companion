c91550f77a38dfa7916271da06fcabd580fe0df0
Manny Silva <hawkeyexl@gmail.com>
2022-11-21 12:55:03 -0800

    Got JS working in injected HTML

--- dist/dialog.js
+++ dist/dialog.js
@@ -3,6 +3,20 @@ var __webpack_exports__ = {};
 /*!***********************!*\
   !*** ./src/dialog.js ***!
   \***********************/
-
+function copySelector() {
+    // Get the text field
+    var copyText = document.getElementById("selectorDisplay");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
 /******/ })()
 ;
+//# sourceMappingURL=dialog.js.map
--- src/display.js
+++ src/display.js
@@ -2,7 +2,7 @@ const dialogBody = `
   <div id="selector" class="code">
     <p id="selectorDisplay">
     </p>
-    <button onclick="copySelector()">
+    <button onclick='${copySelector}; copySelector();'>
       <svg style="width:24px;height:24px" viewBox="0 0 24 24">
         <path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
       </svg>
@@ -21,6 +21,21 @@ async function toggleDisplay() {
     // If exists, remove it
     dialog.remove();
   }
+}
+
+function copySelector() {
+  // Get the text field
+  var copyText = document.getElementById("selectorDisplay");
+  console.log(copyText);
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
 }
 
 toggleDisplay();
