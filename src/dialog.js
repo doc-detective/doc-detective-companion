ee41aaecf6ba6ddc1a63600a298072d267863531
manuelrbsilva <48723033+manuelrbsilva@users.noreply.github.com>
2022-12-21 14:40:20 -0800

    Reimplemented the copy functionality.

--- src/dialog.js
+++ src/dialog.js
@@ -2,7 +2,7 @@ const dialogBody = `
   <div id="selector" class="code">
     <p id="selectorDisplay">
     </p>
-    <button class="copy" onclick='${copySelector}; copySelector();'>
+    <button class="copy">
       <svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
       </svg>
@@ -17,6 +17,10 @@ async function toggleDisplay() {
     dialog.id = "doc-detective";
     dialog.innerHTML = dialogBody;
     document.body.appendChild(dialog);
+    const buttons = document.querySelectorAll("#doc-detective .copy");
+    for (const button of buttons) {
+      button.addEventListener("click", () => { copySelector(); })
+    }
   } else {
     // If exists, remove it
     dialog.remove();
