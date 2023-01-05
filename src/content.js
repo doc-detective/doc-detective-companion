b5df4bcbd4888e911fadd66e4d2a0db7fa70e96e
manuelrbsilva <48723033+manuelrbsilva@users.noreply.github.com>
2023-01-05 14:38:55 -0800

    Build out storage fields

--- src/content.js
+++ src/content.js
@@ -1,15 +1,56 @@
 var browser = require("webextension-polyfill");
 import { finder } from "@medv/finder";
 
+function splitAndTrim(string) {
+  if (!string) return [];
+  let result = string.split(",").map(element => element.trim()).filter(element => element !== '');
+  return result;
+}
+
+async function loadStorage() {
+  let storage = await browser.storage.sync.get(
+    [
+      "defaultBehaviorIDs",
+      "defaultBehaviorClasses",
+      "defaultBehaviorTags",
+      "defaultBehaviorAttributes",
+      "allowedIDs",
+      "allowedClasses",
+      "allowedTags",
+      "allowedAttributes",
+      "disallowedIDs",
+      "disallowedClasses",
+      "disallowedTags",
+      "disallowedAttributes"
+    ]
+  );
+  storage.defaultBehaviorIDs = (storage.defaultBehaviorIDs === "true") ? true : false;
+  storage.defaultBehaviorClasses = (storage.defaultBehaviorClasses === "true") ? true : false;
+  storage.defaultBehaviorTags = (storage.defaultBehaviorTags === "true") ? true : false;
+  storage.defaultBehaviorAttributes = (storage.defaultBehaviorAttributes === "true") ? true : false;
+  storage.allowedIDs = splitAndTrim(storage.allowedIDs);
+  storage.allowedClasses = splitAndTrim(storage.allowedClasses);
+  storage.allowedTags = splitAndTrim(storage.allowedTags);
+  storage.allowedAttributes = splitAndTrim(storage.allowedAttributes);
+  storage.disallowedIDs = splitAndTrim(storage.disallowedIDs);
+  storage.disallowedClasses = splitAndTrim(storage.disallowedClasses);
+  storage.disallowedTags = splitAndTrim(storage.disallowedTags);
+  storage.disallowedAttributes = splitAndTrim(storage.disallowedAttributes);
+  return storage;
+}
+
 document.addEventListener("click", async (event) => {
   let dialog = document.getElementById("doc-detective");
   if (dialog) {
+    let storage = await loadStorage();
+    console.log(storage);
     let options = {
       root: document.body,
-      idName: (name) => true,
-      className: (name) => true,
-      tagName: (name) => true,
-      attr: (name, value) => false,
+      // TODO: Refine logic
+      idName: (name) => storage.allowedIDs.includes(name) && !storage.disallowedIDs.includes(name) || storage.defaultBehaviorIDs,
+      className: (name) => storage.allowedClasses.includes(name) && !storage.disallowedClasses.includes(name)|| storage.defaultBehaviorClasses,
+      tagName: (name) => storage.allowedTags.includes(name) && !storage.disallowedTags.includes(name) || storage.defaultBehaviorTags,
+      attr: (name, value) => storage.allowedAttributes.includes(name) && !storage.disallowedAttributes.includes(name) || storage.defaultBehaviorAttributes,
       seedMinLength: 1,
       optimizedMinLength: 2,
       threshold: 1000,
--- src/options.js
+++ src/options.js
@@ -3,26 +3,57 @@ var browser = require("webextension-polyfill");
 function saveOptions(e) {
     e.preventDefault();
     browser.storage.sync.set({
-        preferredID: document.querySelector("#preferredID").value,
-        preferredClass: document.querySelector("#preferredClass").value,
-        preferredTag: document.querySelector("#preferredTag").value,
-        preferredAttribute: document.querySelector("#preferredAttribute").value
+        defaultBehaviorIDs: document.querySelector("#defaultBehaviorIDs").value,
+        defaultBehaviorClasses: document.querySelector("#defaultBehaviorClasses").value,
+        defaultBehaviorTags: document.querySelector("#defaultBehaviorTags").value,
+        defaultBehaviorAttributes: document.querySelector("#defaultBehaviorAttributes").value,
+        allowedIDs: document.querySelector("#allowedIDs").value,
+        allowedClasses: document.querySelector("#allowedClasses").value,
+        allowedTags: document.querySelector("#allowedTags").value,
+        allowedAttributes: document.querySelector("#allowedAttributes").value,
+        disallowedIDs: document.querySelector("#disallowedIDs").value,
+        disallowedClasses: document.querySelector("#disallowedClasses").value,
+        disallowedTags: document.querySelector("#disallowedTags").value,
+        disallowedAttributes: document.querySelector("#disallowedAttributes").value
     });
 }
 
 function restoreOptions() {
     function setCurrentChoice(result) {
-        document.querySelector("#preferredID").value = result.preferredID || "";
-        document.querySelector("#preferredClass").value = result.preferredClass || "";
-        document.querySelector("#preferredTag").value = result.preferredTag || "";
-        document.querySelector("#preferredAttribute").value = result.preferredAttribute || "";
+        document.querySelector("#defaultBehaviorIDs").value = result.defaultBehaviorIDs || "true";
+        document.querySelector("#defaultBehaviorClasses").value = result.defaultBehaviorClasses || "true";
+        document.querySelector("#defaultBehaviorTags").value = result.defaultBehaviorTags || "true";
+        document.querySelector("#defaultBehaviorAttributes").value = result.defaultBehaviorAttributes || "false";
+        document.querySelector("#allowedIDs").value = result.allowedIDs || "";
+        document.querySelector("#allowedClasses").value = result.allowedClasses || "";
+        document.querySelector("#allowedTags").value = result.allowedTags || "";
+        document.querySelector("#allowedAttributes").value = result.allowedAttributes || "";
+        document.querySelector("#disallowedIDs").value = result.disallowedIDs || "";
+        document.querySelector("#disallowedClasses").value = result.disallowedClasses || "";
+        document.querySelector("#disallowedTags").value = result.disallowedTags || "";
+        document.querySelector("#disallowedAttributes").value = result.disallowedAttributes || "";
     }
 
     function onError(error) {
         console.log(`Error: ${error}`);
     }
 
-    let getting = browser.storage.sync.get(["preferredID", "preferredClass", "preferredTag", "preferredAttribute"]);
+    let getting = browser.storage.sync.get(
+        [
+            "defaultBehaviorIDs",
+            "defaultBehaviorClasses",
+            "defaultBehaviorTags",
+            "defaultBehaviorAttributes",
+            "allowedIDs",
+            "allowedClasses",
+            "allowedTags",
+            "allowedAttributes",
+            "disallowedIDs",
+            "disallowedClasses",
+            "disallowedTags",
+            "disallowedAttributes"
+        ]
+    );
     getting.then(setCurrentChoice, onError);
 }
 
