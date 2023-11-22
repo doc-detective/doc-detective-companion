2b2f4ecf58f1b761f96c2f8762fe7b3f65d3db7d
hawkeyexl <hawkeyexl@gmail.com>
2023-11-22 13:09:58 -0800

    Refactor panel width in togglePanel function

--- src/content.js
+++ src/content.js
@@ -59,15 +59,16 @@ browser.runtime.onMessage.addListener((message) => {
 
 function togglePanel() {
   const id = "doc-detective";
+  const width = 350;
   let panel = document.getElementById(id);
   if (panel) {
     panel.remove();
     const margin = document.body.style.marginRight.replace("px", "");
-    document.body.style.marginRight = margin ? `${margin - 250}px` : "0px";
+    document.body.style.marginRight = margin ? `${margin - width}px` : "0px";
   } else {
     panel = document.createElement("div");
     panel.id = id;
-    panel.style = "position: fixed; top: 0; right: 0; width: 250px; height: 100vh; overflow-y: auto; background-color: white; padding: 20px; box-shadow: 2px 0 5px rgba(0,0,0,0.5);";
+    panel.style = "position: fixed; z-index: 1000; top: 0; right: 0; width: 250px; height: 100vh; overflow-y: auto; background-color: white; box-shadow: 2px 0 5px rgba(0,0,0,0.5);";
     document.body.appendChild(panel);
     // Run bundle.js
     var script = document.createElement("script");
@@ -76,7 +77,7 @@ function togglePanel() {
       this.remove();
     };
     const margin = document.body.style.marginRight.replace("px", "");
-    document.body.style.marginRight = margin ? `${margin + 250}px` : "250px";
+    document.body.style.marginRight = margin ? `${margin + width}px` : `${width}px`;
     (document.head || document.documentElement).appendChild(script);
   }
 }
