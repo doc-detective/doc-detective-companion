89c8824bc159b6e199f1c77abbfad352ee29aac6
hawkeyexl <hawkeyexl@gmail.com>
2023-11-24 10:50:59 -0800

    Added close panel functionality button

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ import SearchIcon from "@mui/icons-material/Search";
 // import PlayCircleIcon from "@mui/icons-material/PlayCircle";
 import SettingsIcon from "@mui/icons-material/Settings";
 import CloseIcon from "@mui/icons-material/Close";
-import HandymanIcon from '@mui/icons-material/Handyman';
+import HandymanIcon from "@mui/icons-material/Handyman";
 
 function App() {
   const [mode, setMode] = useState("search");
@@ -25,23 +25,60 @@ function App() {
     }
   };
 
+  const closePanel = () => {
+    const id = "doc-detective";
+    const width = 350;
+    let panel = document.getElementById(id);
+    const margin = document.body.style.marginRight.replace("px", "");
+    document.body.style.marginRight = margin ? `${margin - width}px` : "0px";
+    panel.remove();
+  };
+
   return (
     <Box>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <Typography variant="h6" style={{flexGrow: 1}}>Doc Detective</Typography>
-          <IconButton color="inherit" style={{margin: "auto"}} edge="end" aria-label="settings">
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Doc Detective
+          </Typography>
+          <IconButton
+            color="inherit"
+            style={{ margin: "auto" }}
+            edge="end"
+            aria-label="settings"
+          >
             <SettingsIcon />
           </IconButton>
-          <IconButton color="inherit" edge="end" aria-label="close">
+          <IconButton
+            color="inherit"
+            edge="end"
+            aria-label="close"
+            onClick={closePanel}
+          >
             <CloseIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
 
-      <Tabs value={mode} disableGutters onChange={handleModeChange} aria-label="Mode" centered>
-        <Tab value="search" style={{flexGrow: 1, maxWidth: '50%'}} icon={<SearchIcon />} label="Search" />
-        <Tab value="build" style={{flexGrow: 1, maxWidth: '50%'}} icon={<HandymanIcon />} label="Build" />
+      <Tabs
+        value={mode}
+        disableGutters
+        onChange={handleModeChange}
+        aria-label="Mode"
+        centered
+      >
+        <Tab
+          value="search"
+          style={{ flexGrow: 1, maxWidth: "50%" }}
+          icon={<SearchIcon />}
+          label="Search"
+        />
+        <Tab
+          value="build"
+          style={{ flexGrow: 1, maxWidth: "50%" }}
+          icon={<HandymanIcon />}
+          label="Build"
+        />
       </Tabs>
 
       <Typography variant="h6" sx={{ marginTop: 2 }}>
